005b2383753ac597bde287703ca2a20b76c4743b
PhuocTri0106 <you@PhuocTri0106.com>
2023-08-11 00:41:13 +0700

    fix server.py and monet_model.py

--- monet_model.py
+++ monet_model.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import os
 
 def model(image):
-    uploaded_file_path = "datasets/images/"+image.filename
+    uploaded_file_path = "/opt/render/project/src/datasets/images/"+image.filename
     image.save(uploaded_file_path)
-    command = "python test.py --dataroot datasets\images --name style_monet_pretrained --model test --no_dropout --gpu_ids -1"
+    command = "python test.py --dataroot /opt/render/project/src/datasets/images --name style_monet_pretrained --model test --no_dropout --gpu_ids -1"
     os.remove(uploaded_file_path)
     try:
         subprocess.run(command, shell=True, check=True)
         filename_fake = image.filename[:-4] + "_fake.png" 
-        style_image_path = "results/style_monet_pretrained/test_latest/images/" + filename_fake
+        style_image_path = "/opt/render/project/src/results/style_monet_pretrained/test_latest/images/" + filename_fake
         style_image = plt.imread(style_image_path)
         print("Command executed successfully.")
         return style_image
