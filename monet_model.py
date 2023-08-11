8e480f751507b683f8d87952110c0aabd45372cc
PhuocTri0106 <you@PhuocTri0106.com>
2023-08-11 09:24:51 +0700

    fix bug

--- monet_model.py
+++ monet_model.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 import os
 
 def model(image):
-    uploaded_file_path = "./opt/render/project/src/datasets/images/"+image.filename
+    # uploaded_file_path = "./opt/render/project/src/datasets/images/"+image.filename
+    image.filename = "image.png"
+    uploaded_file_path = "./opt/render/project/src/datasets/images/image.png"
     image.save(uploaded_file_path)
     command = "python test.py --dataroot ./opt/render/project/src/datasets/images --name style_monet_pretrained --model test --no_dropout --gpu_ids -1"
     try:
         subprocess.run(command, shell=True, check=True)
-        filename_fake = image.filename[:-4] + "_fake.png" 
-        style_image_path = "./opt/render/project/src/results/style_monet_pretrained/test_latest/images/" + filename_fake
+        style_image_path = "./opt/render/project/src/results/style_monet_pretrained/test_latest/images/image_fake.png"
         style_image = plt.imread(style_image_path)
         print("Command executed successfully.")
         # os.remove(uploaded_file_path)
