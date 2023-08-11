137ddb370e014293ad181bb09a4e54351007a3dc
PhuocTri0106 <you@PhuocTri0106.com>
2023-08-11 22:42:44 +0700

    return image

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, send_file
 import os
 from monet_model import monet_style
 from vangogh_model import vangogh_style
@@ -18,8 +18,15 @@ def monet():
         monet_style(f)
         style_image_path = "results/style_monet_pretrained/test_latest/images/image_fake.png"
         # protocol = request.headers.get('X-Forwarded-For', request.scheme)
-        style_image_url = url_for('static', filename=style_image_path, _external=True)
-        return jsonify({'image_url': style_image_url})
+        # style_image_url = url_for('static', filename=style_image_path, _external=True)
+        # return jsonify({'image_url': style_image_url})
+        style_image = plt.imread(style_image_path)
+        fig = plt.figure()
+        plt.imshow(style_image)
+        plt.axis('off')
+        plt.savefig('result.png')
+        plt.close(fig)
+        return send_file('result.png', mimetype='image/png')
 
 @app.route("/vangogh", methods=['POST'])
 def vangogh():
@@ -27,9 +34,13 @@ def vangogh():
     if f.filename != "":
         vangogh_style(f)
         style_image_path = "results/style_vangogh_pretrained/test_latest/images/image_fake.png"
-        # protocol = request.headers.get('X-Forwarded-For', request.scheme)
-        style_image_url = url_for('static', filename=style_image_path, _external=True)
-        return jsonify({'image_url': style_image_url})
+        style_image = plt.imread(style_image_path)
+        fig = plt.figure()
+        plt.imshow(style_image)
+        plt.axis('off')
+        plt.savefig('result.png')
+        plt.close(fig)
+        return send_file('result.png', mimetype='image/png')
 
 @app.route("/cezanne", methods=['POST'])
 def cezanne():
@@ -37,9 +48,13 @@ def cezanne():
     if f.filename != "":
         cezanne_style(f)
         style_image_path = "results/style_cezanne_pretrained/test_latest/images/image_fake.png"
-        # protocol = request.headers.get('X-Forwarded-For', request.scheme)
-        style_image_url = url_for('static', filename=style_image_path, _external=True)
-        return jsonify({'image_url': style_image_url})
+        style_image = plt.imread(style_image_path)
+        fig = plt.figure()
+        plt.imshow(style_image)
+        plt.axis('off')
+        plt.savefig('result.png')
+        plt.close(fig)
+        return send_file('result.png', mimetype='image/png')
     
 @app.route("/ukiyoe", methods=['POST'])
 def ukiyoe():
@@ -47,10 +62,13 @@ def ukiyoe():
     if f.filename != "":
         ukiyoe_style(f)
         style_image_path = "results/style_ukiyoe_pretrained/test_latest/images/image_fake.png"
-        # protocol = request.headers.get('X-Forwarded-For', request.scheme)
-        # style_image_url = url_for('static', filename=style_image_path, _external=True, _scheme=protocol)
-        style_image_url = url_for('static', filename=style_image_path, _external=True)
-        return jsonify({'image_url': style_image_url})
+        style_image = plt.imread(style_image_path)
+        fig = plt.figure()
+        plt.imshow(style_image)
+        plt.axis('off')
+        plt.savefig('result.png')
+        plt.close(fig)
+        return send_file('result.png', mimetype='image/png')
     
 if __name__ == "__main__":
     app.run(debug=True)
