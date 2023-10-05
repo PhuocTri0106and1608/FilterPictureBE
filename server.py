32f0f3d32e2f99465af61eabcb7aec1f34dc1cab
PhuocTri0106 <you@PhuocTri0106.com>
2023-10-05 23:45:07 +0700

    add more route

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ from waitress import serve
 app = Flask(__name__)
 
 
-@app.route("/transfer", methods=['POST'])
-def transfer():
-    f = request.files['upload']
+@app.route("/all", methods=['POST'])
+def all():
+    f = request.files['upload_all']
     if f.filename != "":
         monet_style(f)
         vangogh_style(f)
@@ -44,6 +44,79 @@ def transfer():
             'image_cezanne_url': style_cezanne_image_url,
             'image_ukiyoe_url': style_ukiyoe_image_url
         })
+    
+@app.route("/Monet", methods=['POST'])
+def Monet():
+    f = request.files['upload_Monet']
+    if f.filename != "":
+        monet_style(f)
+
+        style_monet_image_path = "results/style_monet_pretrained/test_latest/images/image_fake.png"
+
+        img_monet = Image.open(style_monet_image_path)
+
+        img_monet.save("static/results/style_monet_pretrained/test_latest/images/image_fake.png", 'PNG')
+
+        style_monet_image_url = url_for('static', filename=style_monet_image_path, _external=True)
+
+        return jsonify({
+            'image_url': style_monet_image_url
+        })
+    
+@app.route("/Vangogh", methods=['POST'])
+def Vangogh():
+    f = request.files['upload_Vangogh']
+    if f.filename != "":
+        vangogh_style(f)
+
+        style_vangogh_image_path = "results/style_vangogh_pretrained/test_latest/images/image_fake.png"
+
+        img_vangogh = Image.open(style_vangogh_image_path)
+
+        img_vangogh.save("static/results/style_vangogh_pretrained/test_latest/images/image_fake.png", 'PNG')
+
+        style_vangogh_image_url = url_for('static', filename=style_vangogh_image_path, _external=True)
+
+        return jsonify({
+            'image_url': style_vangogh_image_url
+        })
+    
+@app.route("/Cezanne", methods=['POST'])
+def Cezanne():
+    f = request.files['upload_Cezanne']
+    if f.filename != "":
+        cezanne_style(f)
+
+        style_cezanne_image_path = "results/style_cezanne_pretrained/test_latest/images/image_fake.png"
+
+        img_cezanne = Image.open(style_cezanne_image_path)
+
+        img_cezanne.save("static/results/style_cezanne_pretrained/test_latest/images/image_fake.png", 'PNG')
+
+        style_cezanne_image_url = url_for('static', filename=style_cezanne_image_path, _external=True)
+
+        return jsonify({
+            'image_url': style_cezanne_image_url
+        })
+    
+@app.route("/Ukiyoe", methods=['POST'])
+def Ukiyoe():
+    f = request.files['upload_Ukiyoe']
+    if f.filename != "":
+        ukiyoe_style(f)
+
+        style_ukiyoe_image_path = "results/style_ukiyoe_pretrained/test_latest/images/image_fake.png"
+
+        img_ukiyoe = Image.open(style_ukiyoe_image_path)
+
+        img_ukiyoe.save("static/results/style_ukiyoe_pretrained/test_latest/images/image_fake.png", 'PNG')
+
+        style_ukiyoe_image_url = url_for('static', filename=style_ukiyoe_image_path, _external=True)
+
+        return jsonify({
+            'image_url': style_ukiyoe_image_url
+        })
+    
 
     
 if __name__ == "__main__":
